867d3a5b95fcb4498a40afd45a477f88002727a3
Kehinde <kehindeayanleye@gmail.com>
2015-02-04 01:41:39 +0100

    button

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,30 @@
+var myImage = document.querySelector('Img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/firefox-icon.png'){
+	myImage.setAttribute ('src', 'images/barak_obama.png');
+	} else {
+	myImage.setAttribute ('src', 'images/firefox-icon.png');
+		}
+}
+
+
+
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Hello world!';
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.innerHTML = 'Who are you ' + storedName + '?';
+}
+
+myButton.onclick = setUserName;
